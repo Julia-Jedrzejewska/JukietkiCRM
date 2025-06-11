fd4b54095a4f523cc96d9bcec2e0fe8873dfbca1
Julka <julia.jedrzejewska@zsp1jarocin.edu.pl>
2025-06-11 15:09:50 +0200

    Routes #1

--- JukietkiCRM/routes/web.php
+++ JukietkiCRM/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ExpenseController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('/Forms/OrderForm');
-});
+// main path
+Route::get('/', function () {return view('main');})->name('main');
+
+// resources
+Route::resource('clients', ClientController::class);
+Route::resource('orders', OrderController::class);
+Route::resource('expenses', ExpenseController::class);
+

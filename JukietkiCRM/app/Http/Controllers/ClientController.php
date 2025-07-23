1b06019d49390c80a6baabfa56ab043055a3611f
Julka <julia.jedrzejewska@zsp1jarocin.edu.pl>
2025-07-23 10:34:57 +0200

    Empty ClientController

--- JukietkiCRM/app/Http/Controllers/ClientController.php
+++ JukietkiCRM/app/Http/Controllers/ClientController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ClientController
+class ClientController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return view('client.index');
+        //
     }
 
     /**

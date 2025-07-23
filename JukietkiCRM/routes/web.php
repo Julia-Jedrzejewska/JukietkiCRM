c440269e92efb942d31aefc13939a6eae339f26b
Julka <julia.jedrzejewska@zsp1jarocin.edu.pl>
2025-07-23 12:32:17 +0200

    routes

--- JukietkiCRM/routes/web.php
+++ JukietkiCRM/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
 // main path
@@ -13,3 +14,8 @@ Route::resource('clients', ClientController::class);
 Route::resource('orders', OrderController::class);
 Route::resource('expenses', ExpenseController::class);
 
+//Route::get('clients/search', [ClientController::class, 'search']);
+
+Route::get('live-search', [SearchController::class, 'index']);
+
+Route::get('search', [SearchController::class, 'liveSearchTable']);
